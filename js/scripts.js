dd08be53df28c67461955a678c664cee9b0b06a6
Amanda Johnson <amandajnej@gmail.com>
2017-06-09 13:34:24 +0100

    Add initial HTML CSS Script.

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $("#orderForm").submit(function(event) {
+    var firstName = $("#firstName").val();
+    var surName = $("#surName").val();
+    var houseNo = $("#houseNo").val();
+    var street = $("#street").val();
+    var town = $("#town").val();
+    var county = $("#county").val();
+    var postCode = $("#postCode").val();
+
+    $(".firstName").text(firstName);
+    $(".surName").text(surName);
+    $(".houseNo").text(houseNo);
+    $(".street").text(street);
+    $(".town").text(town);
+    $(".county").text(county);
+    $(".postCode").text(postCode);
+
+
+    $(".receipt").show();
+
+  $(".btn1").click(function() {
+    $(".confirm").show();
+  })
+
+    event.preventDefault();
+  })
+})
